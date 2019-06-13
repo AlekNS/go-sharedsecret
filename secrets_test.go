6ebf719558099512706bb1f559ab2f7c2f0bc30a
Alexander Sedelnikov <alexander.sedelnikov@gmail.com>
2019-06-13 23:27:00 +0700

    Fix tests. Fix randgen.

--- secrets_test.go
+++ secrets_test.go
@@ -9,7 +9,7 @@ func TestShareSecretByShamirSchema(t *testing.T) {
 		secCtx := SecretContext{
 			Bits:      8,
 			Radix:     16,
-			MaxShares: 256,
+			MaxShares: 128,
 		}
 
 		sh := &shareSecretByShamirSchema{
@@ -17,8 +17,13 @@ func TestShareSecretByShamirSchema(t *testing.T) {
 			secCtx:    secCtx,
 		}
 		sh.init(secCtx.Bits)
-		got, err := sh.Share("112233445566", 3, 2, 32)
-		t.Errorf("%#v %v\n", got, err)
+		got, err := sh.Share("12345678", 3, 2, 128)
+		if err != nil {
+			t.Errorf("expect no error, got %v", err)
+		}
+		if len(got) != 3 {
+			t.Errorf("expect 3 parts, got %v", got)
+		}
 	})
 
 	// []string{"8015a01113e0c525543", "802b401111a4d68552c", "803ee011106727e5509"}
@@ -29,7 +34,7 @@ func TestShareSecretByShamirSchemaCombine(t *testing.T) {
 		secCtx := SecretContext{
 			Bits:      8,
 			Radix:     16,
-			MaxShares: 256,
+			MaxShares: 128,
 		}
 
 		sh := &shareSecretByShamirSchema{
@@ -37,8 +42,12 @@ func TestShareSecretByShamirSchemaCombine(t *testing.T) {
 			secCtx:    secCtx,
 		}
 		sh.init(secCtx.Bits)
-		got, err := sh.Combine([]string{"8015a01113e0c525543", "802b401111a4d68552c", "803ee011106727e5509"}, 0)
-		t.Errorf("%#v %v\n", got, err)
-		//
+		got, err := sh.Combine([]string{"801e58700a2578c8757c486c3d17b6601aa", "802d7130059ae0513ae95119bbcc090f8c1", "803329400fbf98994f95197586ca9c2af13"}, 0)
+		if err != nil {
+			t.Errorf("expect no error, got %v", err)
+		}
+		if got != "12345678" {
+			t.Errorf("expect 12345678, got %v", got)
+		}
 	})
 }
